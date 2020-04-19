0f0bbe9193fe22fe6dc6abf4a2f269cc9f21e67a
deep1401 <gandhi0869@gmail.com>
2020-04-19 15:56:54 +0530

    Add basic follow and comment functionality

--- instapy-test.py
+++ instapy-test.py
@@ -2,4 +2,19 @@ from instapy import InstaPy
 from selenium import webdriver
 username=input("Enter username: ")
 password=input("Enter password: ")
-InstaPy(username=username,password=password).login()
+session=InstaPy(username=username,password=password)
+#Preventing bot detection
+session.set_quota_supervisor(enabled=True,peak_comments_daily=240, peak_comments_hourly=21)
+#Logging into the session
+session.login()
+#Liking based on usernames
+session.like_by_users(['instagram','design'],amount=5)
+#Preventing spam likes
+session.set_dont_like(["naked", "nsfw"])
+#Follow all users whose images were liked
+session.set_do_follow(True,percentage=100)
+session.set_do_comment(True, percentage=67)
+session.set_comments(['This is some amazing content',"Wow","Nice posts!"])
+session.end()
+session.set_quota_supervisor(enabled=True)
+
